a3c4a79b89d45e4d7522efa55c52a7d134de7174
Nandan Rao <nandanmarkrao@gmail.com>
2018-01-19 23:52:21 +0100

    Made cluster weeks a env var.

--- __main__.py
+++ __main__.py
@@ -9,7 +9,9 @@ from datetime import datetime, timedelta
 if __name__ == '__main__':
     client = MongoClient(host = os.environ.get('MONGO_HOST') or None)
     collection = client['newsfilter'].news
-    get_from = datetime.utcnow() - timedelta(weeks = 1) # how long??
-    write_predictions(collection, get_from)
-    write_clusters(collection, get_from)
+    cluster_weeks = os.environ.get('ORACLE_CLUSTER_WEEKS') or 3
+    cluster_from = datetime.utcnow()- timedelta(weeks = cluster_weeks)
+    predict_from = datetime.utcnow() - timedelta(weeks = 3)
+    write_predictions(collection, predict_from)
+    write_clusters(collection, cluster_from)
     client.close()
