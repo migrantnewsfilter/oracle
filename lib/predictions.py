3baa0c31ec3d9c01a558325c0b9b1392c935cd14
Nandan Rao <nandanmarkrao@gmail.com>
2018-01-21 21:01:16 +0100

    Updated write_predictions to use new modelling api

--- lib/predictions.py
+++ lib/predictions.py
@@ -3,7 +3,7 @@ from toolz import assoc
 from pymongo import UpdateOne
 import pandas as pd
 import math
-from modelling.models import create_model
+from modelling.models import get_prediction_data, train_and_predict
 from modelling.utils import get_articles
 # from modelling.clustering import get_unique_items
 from modelling.fetch import create_df, get_labelled_articles
@@ -14,55 +14,27 @@ def chunk(n, it):
     src = iter(it)
     return takewhile(bool, (list(islice(src, n)) for _ in count(0)))
 
-def make_prediction(item, model):
-    try:
-        body = item['content']['body']
-        return model.predict_proba([body])[0][0]
-    except KeyError:
-        return None
-
 def normalize_prediction(prediction):
     return int(math.floor(prediction*10))
 
-def predict_item(item, model):
-    prediction = normalize_prediction(make_prediction(item, model))
-    return assoc(item, 'prediction', prediction)
-
-def get_model(collection):
-    logging.debug('Getting data for model for prediction')
-
-    # TODO: ge --> with title or body??
-    lookup = [('ge', 'title'),
-              ('tw', 'body'),
-              ('fa', 'body')]
-
-    articles = get_articles(collection, label = True, unique = True)
-    articles = list(articles)
-    if not articles:
-        raise Exception('Could not find any labelled articles in Database')
-
-    df = create_df(articles)
-    logging.debug('Creating model for prediction')
-    return create_model(df.body, df.label, [0.5,0.5])
-
 def write_predictions(collection, get_from):
     # If we can't make a model, don't write any predictions
-    try:
-        model = get_model(collection)
-    except Exception as e:
-        logging.exception('Error creating model for predictions: {}'.format(e))
-        return
+    X_train, y_train, _ = get_prediction_data(collection, label=True)
+    X_new, _, ids = get_prediction_data(collection, label=True, start=get_from)
 
     logging.debug('Predicting items.')
-    unlabelled = get_articles(collection, label = False, date_start = get_from, unique = True)
-    predicted = (predict_item(item, model) for item in unlabelled)
-    chunked = chunk(200, predicted)
+    try:
+        preds = train_and_predict(X_train, y_train, X_new)
+    except Exception as e:
+        logging.error('Error predicting data: ', e)
+        return
 
+    chunked = chunk(200, zip(ids, map(normalize_prediction, preds)))
     for c in chunked:
         logging.debug("Writing prediction update to DB")
-        requests = [ UpdateOne({ '_id': obj['_id']},
-                               {'$set': { 'prediction': obj['prediction'] }})
-                     for obj in c ]
+        requests = [ UpdateOne({ '_id': i},
+                               {'$set': { 'prediction': p }})
+                     for i,p in c ]
         collection.bulk_write(requests, ordered = False)
 
 def write_clusters(collection, get_from, cluster_window):
--- lib/test_predictions.py
+++ lib/test_predictions.py
@@ -4,6 +4,7 @@ from mongomock import MongoClient
 from pymongo import UpdateOne, MongoClient as MC
 from datetime import datetime
 import pandas as pd
+import numpy as np
 import pytest
 
 @pytest.fixture(scope="module")
@@ -16,20 +17,17 @@ def collection():
 
 
 def test_write_predictions_creates_model_and_writes_only_unlabelled(collection):
-    model = MagicMock()
-    model.predict_proba = MagicMock(return_value = [[.3]])
-    create_model = MagicMock(return_value = model)
-    with patch('lib.predictions.create_model', create_model) as create_model:
+    m = MagicMock(return_value = np.array([.34]))
+    with patch('lib.predictions.train_and_predict', m) as tp:
         collection.insert_many([
-            {'_id': 'tw:abc', 'title': None, 'content': {'body': 'foo goes to a bar'}, 'label': 'shite', 'added': datetime.utcnow()},
+            {'_id': 'tw:abc', 'title': None, 'content': {'body': 'foo goes to a bar'}, 'label': 'accepted', 'added': datetime.utcnow()},
             {'_id': 'tw:cde', 'title': None, 'content': {'body': 'bar walks down the street'}, 'added': datetime.utcnow()}
         ])
         collection.bulk_write = MagicMock()
         write_predictions(collection, datetime(1970,1,1))
-        model_data = create_model.call_args[0]
+        model_data = tp.call_args[0]
         assert model_data[0].values == ['foo goes to a bar']
-        assert model_data[1].values == ['shite']
-        model.predict_proba.assert_called_once()
+        assert model_data[1].values == [True]
         updates = collection.bulk_write.call_args[0][0]
         assert len(updates) == 1
         assert updates[0]._doc['$set'] == {'prediction': 3}
@@ -39,14 +37,14 @@ def test_write_predictions_creates_model_and_writes_only_unlabelled(collection):
 def test_write_predictions_create_model_exception(collection):
     model = MagicMock()
     model.predict_proba = MagicMock(return_value = [[.3]])
-    create_model = MagicMock(side_effect = ValueError())
-    with patch('lib.predictions.create_model', create_model) as create_model:
+    train_and_predict = MagicMock(side_effect = ValueError())
+    with patch('lib.predictions.train_and_predict', train_and_predict) as tp:
         collection.insert_many([
             {'_id': 'tw:cde', 'cluster': 'blah', 'title': 'foo', 'content': {'body': 'bar'}, 'label': 'rejected', 'added': datetime.utcnow()}
         ])
         collection.bulk_write = MagicMock()
         write_predictions(collection, datetime(1970,1,1))
-        create_model.assert_called_once()
+        tp.assert_called_once()
         model.predict_proba.assert_not_called()
         collection.bulk_write.assert_not_called()
         collection.drop()
